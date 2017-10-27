d1f5c679359c6acf668e4c3ce6a9b2010f654427
Anthony Mengel <anthonymengel1232@gmail.com>
2017-10-26 20:56:24 -0400

    removed excess code the wall is gone

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ function handleFormSubmit() {
         let searchterm = $(this).find(".js-input").val();
         $(this).find(".js-input").val("");
         STORE.searchterm = searchterm;
-        if($(this).hasClass('form-homepage')) {
+        if ($(this).hasClass('form-homepage')) {
             $(".js-start").addClass("hidden");
             $(".js-results").removeClass("hidden");
-        } 
+        }
         createApiRequest(searchterm);
     });
 }
@@ -41,11 +41,11 @@ function createApiRequest(searchterm) {
 function createHtml() {
     let string = '';
     if (STORE.currentsetting === 'today') {
-        string = createToday();
+        string = generateToday();
     } else if (STORE.currentsetting === '3Day') {
-        string = create3Day();
+        string = generateDay([0, 7, 15]);
     } else if (STORE.currentsetting === '5Day') {
-        string = create5Day();
+        string = generateDay([0, 7, 15, 23, 30]);
     } else { console.log('There was an error') }
     renderHtml(string);
 }
@@ -54,25 +54,25 @@ function renderHtml(string) {
     $('.js-results-container').html(string);
 }
 
-function toTitleCase(str){
-    return str.replace(/\w\S*/g, function(txt){
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
 
-function createDate(rawdata){
-    let dataArr = rawdata.split(" "); 
-    smallData = dataArr[0].split("-"); 
-    let year = smallData[0]; 
-    let month = smallData[1]; 
-    switch(month){
+function createDate(rawdata) {
+    let dataArr = rawdata.split(" ");
+    smallData = dataArr[0].split("-");
+    let year = smallData[0];
+    let month = smallData[1];
+    switch (month) {
         case '01':
             month = "January";
             break;
         case '02':
             month = "February";
             break;
-        case'03':
+        case '03':
             month = "March";
             break;
         case '04':
@@ -102,19 +102,20 @@ function createDate(rawdata){
         case '12':
             month = "December";
             break;
-        default: 
-            break;       
-    }; 
-    let day = smallData[2]; 
-    if (day.charAt(0)==="0"){
-        day = day.charAt(1); 
+        default:
+            break;
+    };
+    let day = smallData[2];
+    if (day.charAt(0) === "0") {
+        day = day.charAt(1);
     } else {
         day = day;
     }
-    return `${month} ${day}, ${year}`;  
+    return `${month} ${day}, ${year}`;
 }
 
-function createToday() {
+// append and add class to html
+function generateToday() {
     return `
     <div class="result-title">
         <h2>${toTitleCase(STORE.searchterm)}</h2>
@@ -130,90 +131,30 @@ function createToday() {
     `
 }
 
-function create3Day() {
-    return `
-        <div class="result-title">
-            <h2>${toTitleCase(STORE.searchterm)}</h2>
-        </div>
-        <div class='today-result result'>
-            <h2 class="today">Today</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[0].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[0].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[0].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[0].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[0].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-        <div class='24hrs-future-result result'>
-            <h2 class="tomorrow">${createDate(STORE.data.list[7].dt_txt)}</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[7].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[7].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[7].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[7].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[7].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-        <div class='48hrs-future-result result'>
-            <h2>${createDate(STORE.data.list[15].dt_txt)}</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[15].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[15].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[15].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[15].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[15].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-            `
-}
+function generateDay(dataArr) {
+    $('.js-results-container').empty();
+    $('.js-results-container').append(`<div class="result-title">
+    <h2>${toTitleCase(STORE.searchterm)}</h2>
+    </div>`);
+    for (let i = 0; i < dataArr.length; i++) {
+        let dayIndex = dataArr[i];
+        $('.js-results-container').append(`
+        <div class='future-result result'>
+        <h2>${createDate(STORE.data.list[dayIndex].dt_txt)}</h2>
+            <img src="http://openweathermap.org/img/w/${STORE.data.list[dayIndex].weather[0].icon}.png" alt+"weather pic" class="weather-icon">
+            <p><b>${toTitleCase(STORE.data.list[dayIndex].weather[0].description)}</b> </p>
+            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[dayIndex].main.temp) * 9 / 5 - 459.67)} &#176F</p>
+            <p><b>Humidity:</b> ${STORE.data.list[dayIndex].main.humidity}%</p>
+            <p><b>Wind:</b> ${Math.floor((STORE.data.list[dayIndex].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
+         </div>
+        `);
+    }
 
-function create5Day() {
-
-    return `
-        <div class="result-title">
-            <h2>${toTitleCase(STORE.searchterm)}</h2>
-        </div>
-        <div class='today-result result'>
-            <h2 class="today">Today</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[0].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[0].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[0].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[0].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[0].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-        <div class='future-result result'>
-            <h2 class="tomorrow">${createDate(STORE.data.list[7].dt_txt)}</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[7].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[7].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[7].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[7].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[7].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-        <div class='future-result result'>
-            <h2>${createDate(STORE.data.list[15].dt_txt)}</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[15].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[15].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[15].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[15].main.humidity}% humidity</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[15].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-        <div class='future-result result'>
-            <h2>${createDate(STORE.data.list[23].dt_txt)}</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[23].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[23].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[23].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[23].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[23].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-        <div class='future-result result'>
-            <h2>${createDate(STORE.data.list[31].dt_txt)}</h2>
-            <img src="http://openweathermap.org/img/w/${STORE.data.list[31].weather[0].icon}.png" alt+"weather pic" class="weather-icon">            
-            <p><b>${toTitleCase(STORE.data.list[31].weather[0].description)}</b> </p>
-            <p><b>Temperature:</b> ${Math.floor((STORE.data.list[31].main.temp) * 9 / 5 - 459.67)} &#176F</p>
-            <p><b>Humidity:</b> ${STORE.data.list[31].main.humidity}%</p>
-            <p><b>Wind:</b> ${Math.floor((STORE.data.list[31].wind.speed) * 60 * 60 * 0.000621371)} mph</p>
-        </div>
-    `
 }
 
 function callback(data) {
     STORE.data = data;
-    createHtml();    
+    createHtml();
 }
 
 function main() {
